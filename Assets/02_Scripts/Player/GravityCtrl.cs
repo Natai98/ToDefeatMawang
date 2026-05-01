c6c781e687ba6d53d30a3f47f4a9a49951d792b2
Natai98 <mrscol0520@naver.com>
2026-05-02 08:32:44 +0900

    Add GravityCtrl

--- Assets/02_Scripts/Player/GravityCtrl.cs
+++ Assets/02_Scripts/Player/GravityCtrl.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
-public class GravityCtrl : MonoBehaviour
+public class GravityControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private CharacterController cc;
+    private Vector3 playerVelocity = Vector3.zero;
+    [SerializeField] private float gravity = -9.8f;
+    private void Awake()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (cc.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        playerVelocity.y += gravity * Time.fixedDeltaTime;
+        cc.Move(playerVelocity * Time.fixedDeltaTime);
     }
 }
